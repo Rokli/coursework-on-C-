990bd0dc5efd523731c9817502f4b19d9fb065e2
satal <satal@DESKTOP-MS43KJ9>
2024-01-31 03:24:17 +0700

    Добавил корявый поворот стрелки

--- Curs/Model/Class/ArrowPanel.cs
+++ Curs/Model/Class/ArrowPanel.cs
@@ -8,9 +8,34 @@ namespace Curs.Model.Class
 {
     public class ArrowPanel : AbstractPanelButton
     {
+        public int RotateAngle { get; set; }
         public ArrowPanel(string FromFile) : base(FromFile)
         {
         }
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            int delta = e.Delta;
 
+            // Поворот кнопки на значение delta
+            RotateAngle += delta/4;
+
+            // Обновление визуального представления кнопки
+            Invalidate();
+
+            base.OnMouseWheel(e);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Создайте экземпляр графики из контекста
+            Graphics graphics = e.Graphics;
+
+            // Поверните графику вокруг центра кнопки
+            graphics.TranslateTransform(Width / 2, Height / 2);
+            graphics.RotateTransform(RotateAngle);
+            graphics.TranslateTransform(-Width / 2, -Height / 2);
+
+            // Вызов базовой реализации
+            base.OnPaint(e);
+        }
     }
 }
